635af8354fea9a94d99c156c242f1e5c9c6d60f0
cllli <94744879+clllihub@users.noreply.github.com>
2024-08-13 10:15:35 +0800

    Update script_bg.js

--- script_bg.js
+++ script_bg.js
@@ -224,7 +224,7 @@ function resetInactivityTimer() {
       currentMediaIndex = videoIndex;
       switchMedia(0);
     }
-  }, 20000); // 20秒后自动播放视频
+  }, 200000); // 200秒后自动播放视频
 }
 
 // 初始化时重置计时器
